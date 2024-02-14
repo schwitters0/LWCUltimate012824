912b4da7c9306180473d63a7cd90406fdfcbba29
schwitters0 <schwitters0@gmail.com>
2024-02-14 08:08:08 -0600

    Commit after DispatchEvents lesson.

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -1,6 +1,16 @@
 import { LightningElement, api } from 'lwc';
 
 export default class MeetingRoom extends LightningElement {
-    //@api meetingRoomInfo;
+    
+    // PROPERTIES
+    @api meetingRoomInfo = {roomName: '', roomCapacity: ''};
     @api showRoomInfo = false;
+
+
+    // METHODS
+    tileClickHandler(){
+        const tileClicked = new CustomEvent('tileclick', {detail : this.meetingRoomInfo, bubbles:true});
+        this.dispatchEvent(tileClicked);
+    }
+
 }
--- force-app/main/default/lwc/meetingRooms/meetingRooms.js
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -1,6 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class MeetingRooms extends LightningElement {
+    
+    // PROPERTIES
+    @track selectedMeetingRoomInfo;
+    
     meetingRoomsInfo = [
         {roomName: 'A-01', roomCapacity: '12'},
         {roomName: 'A-02', roomCapacity: '20'},
@@ -9,4 +13,19 @@ export default class MeetingRooms extends LightningElement {
         {roomName: 'B-02', roomCapacity: '5'},
         {roomName: 'C-01', roomCapacity: '30'}
     ];
+
+    
+    // CONSTRUCTORS
+    constructor(){
+        super();
+        this.template.addEventListener('tileclick', this.onTileSelectHandler.bind(this));
+    }
+    
+    
+    // METHODS
+    onTileSelectHandler(event){
+        const meetingRoomInfo = event.detail;
+        this.selectedMeetingRoomInfo = meetingRoomInfo.roomName;
+    }
+
 }
